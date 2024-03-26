80065795793d5d5e09be89cf0a290ebdadf1b69b
Subrat Meher <subratmeher00000@gmail.com>
2024-03-26 17:39:29 +0530

    added transformers

--- rag.py
+++ rag.py
@@ -1,6 +1,6 @@
 from langchain_community.vectorstores.qdrant import Qdrant
 from langchain_community.chat_models import ChatOllama
-from langchain_community.embeddings import FastEmbedEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.schema.output_parser import StrOutputParser
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -15,7 +15,7 @@ class ChatPDF:
     chain = None
 
     def __init__(self):
-        self.model = ChatOllama(model="mistral",base_url="https://5f47-34-125-218-107.ngrok-free.app")
+        self.model = ChatOllama(model="mistral",base_url="https://0447-104-198-34-110.ngrok-free.app")
         self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=100)
         self.prompt = PromptTemplate.from_template(
             """
@@ -32,8 +32,10 @@ class ChatPDF:
         docs = PyPDFLoader(file_path=pdf_file_path).load()
         chunks = self.text_splitter.split_documents(docs)
         chunks = filter_complex_metadata(chunks)
-
-        vector_store = Qdrant.from_documents(documents=chunks, embedding=FastEmbedEmbeddings(),url="https://2f855f0f-1a16-435e-b0c6-6778fab46d1b.us-east4-0.gcp.cloud.qdrant.io",api_key="WiPEu3ZkXJg-n2KH9nhEZtPvvrWq9VOBUmQy9VS2Tb8TZzY5bFAQ2w",collection_name="mosaic")
+        embeddings = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-mpnet-base-v2"
+        )
+        vector_store = Qdrant.from_documents(documents=chunks, embedding=embeddings,url="https://2f855f0f-1a16-435e-b0c6-6778fab46d1b.us-east4-0.gcp.cloud.qdrant.io",api_key="WiPEu3ZkXJg-n2KH9nhEZtPvvrWq9VOBUmQy9VS2Tb8TZzY5bFAQ2w",collection_name="mosaic")
         self.retriever = vector_store.as_retriever(
             search_type="similarity_score_threshold",
             search_kwargs={
