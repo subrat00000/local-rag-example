94e6f22d927b4b54de78d35e32d60824682561f0
Subrat Meher <subratmeher00000@gmail.com>
2024-03-26 21:08:39 +0530

    fix rag

--- rag.py
+++ rag.py
@@ -7,15 +7,21 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.prompts import PromptTemplate
 from langchain.vectorstores.utils import filter_complex_metadata
-
+from qdrant_client import QdrantClient
+from sentence_transformers import SentenceTransformer
 
 class ChatPDF:
     vector_store = None
     retriever = None
     chain = None
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-mpnet-base-v2"
+    )
 
     def __init__(self):
-        self.model = ChatOllama(model="mistral",base_url="https://0447-104-198-34-110.ngrok-free.app")
+        self.model = ChatOllama(model="mistral",base_url="https://ec7d-34-171-191-121.ngrok-free.app")
+        self.client = QdrantClient(url="https://2f855f0f-1a16-435e-b0c6-6778fab46d1b.us-east4-0.gcp.cloud.qdrant.io",api_key="WiPEu3ZkXJg-n2KH9nhEZtPvvrWq9VOBUmQy9VS2Tb8TZzY5bFAQ2w")
+        self.qdrant = Qdrant(client=self.client,collection_name="mosaic",embeddings=self.embeddings)
         self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=100)
         self.prompt = PromptTemplate.from_template(
             """
@@ -32,26 +38,21 @@ class ChatPDF:
         docs = PyPDFLoader(file_path=pdf_file_path).load()
         chunks = self.text_splitter.split_documents(docs)
         chunks = filter_complex_metadata(chunks)
-        embeddings = HuggingFaceEmbeddings(
-            model_name="sentence-transformers/all-mpnet-base-v2"
-        )
-        vector_store = Qdrant.from_documents(documents=chunks, embedding=embeddings,url="https://2f855f0f-1a16-435e-b0c6-6778fab46d1b.us-east4-0.gcp.cloud.qdrant.io",api_key="WiPEu3ZkXJg-n2KH9nhEZtPvvrWq9VOBUmQy9VS2Tb8TZzY5bFAQ2w",collection_name="mosaic")
-        self.retriever = vector_store.as_retriever(
-            search_type="similarity_score_threshold",
+        Qdrant.add_documents(self=self.qdrant,documents=chunks)
+
+        
+
+    def ask(self, query: str):
+        self.retriever = self.qdrant.as_retriever(
+            search_type="similarity",
             search_kwargs={
-                "k": 3,
-                "score_threshold": 0.5,
+                "k": 5,
             },
         )
-
         self.chain = ({"context": self.retriever, "question": RunnablePassthrough()}
                       | self.prompt
                       | self.model
                       | StrOutputParser())
-
-    def ask(self, query: str):
-        if not self.chain:
-            return "Please, add a PDF document first."
 
         return self.chain.invoke(query)
 
